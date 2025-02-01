2f0abff4ac48b625fa90fdf3ed74496081cb21c6
Jishnuc07 <137303708+Jishnuc07@users.noreply.github.com>
2025-02-01 18:28:27 +0530

    Update Frontend Deploy 4

--- Frontend/src/Api/axios.js
+++ Frontend/src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: process.env.VITE_API_URL
+    baseURL: "https://credit-card-tracker-backend-2-1-django.onrender.com"
 })
 
 api.interceptors.request.use(
